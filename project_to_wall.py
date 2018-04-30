70f83268cb770a19746d836f3ba11033ff1cea08
torbjkri <torbjorn.kringeland88@gmail.com>
2018-04-30 12:03:54 +0200

    minor changes

--- project_to_wall.py
+++ project_to_wall.py
@@ -29,11 +29,11 @@ MIN_MATCH_COUNT = 20
 
 cap = cv2.VideoCapture(1)
 
-img1 = cv2.imread('Figures/QR_real.png',0)
+QR_template = cv2.imread('Figures/QR_real.png',0)
 
 screenshot = cm.screenshot(cap)
 
-img2 = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
+target_scene = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
 
 img4 = cv2.imread('Figures/banksy.jpg',0)
 #screenshot = cm.screenshot(cap)
@@ -44,7 +44,7 @@ cv2.namedWindow('image')
 cv2.setMouseCallback('image',placeImage)
 
 while(1):
-    cv2.imshow('image', img2)
+    cv2.imshow('image', target_scene)
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
         break
@@ -53,76 +53,26 @@ while(1):
 
 sift = cv2.xfeatures2d.SIFT_create()
 
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+kp_template, des_template = sift.detectAndCompute(QR_template,None)
+kp_target, des_target = sift.detectAndCompute(target_scene,None)
 
 
 FLANN_INDEX_KDTREE = 0
-
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks = 50)
 
 flann = cv2.FlannBasedMatcher(index_params, search_params)
 
-img3 = None
-
-true = 0
-
-
-
-    
-#ret, frame = cap.read()
-
-
-
-good = []
-#img2 = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-
-matches = flann.knnMatch(des1,des2,k = 2)
-
-for m,n in matches:
-    if m.distance < 0.7*n.distance:
-        good.append(m)
+goodMatches = pr.findGoodMatches(des_template, des_target, flann)
         
     
-if len(good) > MIN_MATCH_COUNT:
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
-    
-    
-    mean = np.mean(dst_pts, axis = 0)
-    
-    difference = (mouseX,mouseY) - mean
-    
-    dst_pts = dst_pts + difference
-    
-    M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-    matchesMask = mask.ravel().tolist()
-    
-    h, w = img4.shape
-    x0 = 0
-    y0 = 0
-    pts = np.float32([ [x0,y0],[x0,y0+h-1], [x0+w-1,y0+h-1],[x0+w-1,y0] ]).reshape(-1,1,2)
-    dst = cv2.perspectiveTransform(pts,M)
-    
-    img2 = cv2.polylines(img2,[np.int32(dst)], True,255,3,cv2.LINE_AA)
-    
-    draw_params = dict(matchColor = (0,255,0),
-                   singlePointColor = None,
-                   matchesMask = matchesMask,
-                   flags = 2)
+if len(goodMatches) > MIN_MATCH_COUNT:
+    M = pr.findHomography(kp_template, kp_target, goodMatches)
 
-
-    img2_bg = pr.projectImage(img2, img4, M)
-    #img5_tot = cv2.bitwise_and(img2_bg, img5)
-#    img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-    
-    
-
+    img2_bg = pr.projectImage(target_scene, img4, M)
     
 else:
-    print('NOt enough matches found - %d/%d',(len(good), MIN_MATCH_COUNT))
+    print('NOt enough matches found - %d/%d',(len(goodMatches), MIN_MATCH_COUNT))
     matchesMask = None
     
 
--- projection.py
+++ projection.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def projectImage(img1, img2, homography, thresh = 10, maxval = 255):
 
@@ -18,4 +19,30 @@ def projectImage(img1, img2, homography, thresh = 10, maxval = 255):
 
     img_12 = cv2.add(img1_bg, img2_fg)
 
-    return img_12
+    return 
+
+
+import numpy as np
+
+
+## Find good matches between images, and return them
+def findGoodMatches(des1, des2, matchDetector):
+    matches = matchDetector.knnMatch(des1, des2, k = 2)
+    goodMatches = []
+    
+    for m, n in matches:
+        if m.distance < 0.7*n.distance:
+            goodMatches.append(m)
+            
+    return goodMatches
+
+
+##Given keypoints in two images and good matches between them, compute
+## homography using RANSAC
+def findHomography(kp1, kp2, goodMatches):
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in goodMatches]).reshape(-1,1,2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in goodMatches]).reshape(-1,1,2)
+    
+    homography, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    
+    return homography
