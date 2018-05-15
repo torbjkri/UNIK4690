9e3a669bf97d19e246b6f02952e883315fcea541
torbjkri <torbjorn.kringeland88@gmail.com>
2018-05-15 16:15:17 +0200

    Proection to QR codes

---
+++ QR_projections.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 14 11:22:28 2018
+
+@author: wubst
+"""
+
+import cv2
+import numpy as np
+import projection as pr
+
+
+
+class Image:
+    def __init__(self,file):
+        self.image = cv2.imread(file)
+        
+class Template:
+    def __init__(self,file,rotated):
+        if rotated: 
+            temp = cv2.imread(file)
+            self.transform, self.image = pr.rotateMatrix(60,temp)
+            self.transformed = True
+        else:
+            self.image = cv2.imread(file)
+            self.transform = None
+            self.transformed = False
+        
+            
+        
+FLANN_INDEX_KDTREE = 0
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=50)
+matchDetector = cv2.FlannBasedMatcher(index_params, search_params)
+#matchDetector = cv2.BFMatcher(cv2.NORM_HAMMING2)
+
+QR_files = ['Figures/QR_real.png','Figures/qrwall2.png','Figures/qrwall1.png']
+painting_files = ['Figures/fakelove.jpg','Figures/monalisa.jpg','Figures/gtalady2.jpg']
+#painting_files = ['Figures/banksy.jpg','Figures/fakelove.jpg']
+
+MIN_MATCH_COUNT = 10
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1080)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1920)
+
+sift = cv2.xfeatures2d.SIFT_create(nOctaveLayers = 5)
+#sift = cv2.ORB_create()
+#sift = cv2.xfeatures2d.SURF_create(400,5,5)
+
+templates = []
+paintings = []
+paintings_gray = []
+
+screenshot = cv2.imread('Figures/.jpg')
+
+testing = False
+
+des_template = []
+kp_template = []
+
+for ii, template in enumerate(QR_files):    
+    
+    if ii == 0:
+        image = Template(template,True)
+    else:
+        image = Template(template,False)
+    kp, des = sift.detectAndCompute(image.image,None)
+    kp_template.append(kp)
+    des_template.append(des)
+    cv2.imshow(str(ii), image.image)    
+    
+    image.image = cv2.cvtColor(image.image.copy(),cv2.COLOR_BGR2GRAY)
+    templates.append(image)
+    
+for painting in painting_files:
+    image = Image(painting)
+    paintings.append(image)
+    image2 = cv2.cvtColor(image.image.copy(),cv2.COLOR_BGR2GRAY)
+    paintings_gray.append(image2)
+
+cv2.namedWindow('result',cv2.WINDOW_NORMAL)
+
+while(1):
+    
+    if testing == True:
+        ret = True
+        scene_target = screenshot
+    else:
+        ret, scene_target = cap.read()
+    
+    
+    if ret:
+        scene_gray = cv2.cvtColor(scene_target, cv2.COLOR_BGR2GRAY)
+        
+        
+        kp_target, des_target = sift.detectAndCompute(scene_gray,None)
+        
+        
+        for ii, template in enumerate(templates):
+            #found_match = False
+            
+            
+            goodMatches = pr.findGoodMatches(des_template[ii], des_target, matchDetector)
+            
+            if len(goodMatches) > MIN_MATCH_COUNT:
+                homography = pr.findHomography(kp_template[ii], kp_target, goodMatches)
+                scene_target = pr.projectImage(scene_target, paintings[ii].image, homography,15,255,template.transform,template.transformed)
+                
+        cv2.imshow('result', scene_target)
+        
+                
+        
+    else:
+        print('Camera Fuckup')
+        
+    
+    k = cv2.waitKey(10) & 0xFF
+    if k == 32:
+        cv2.destroyAllWindows()
+        cap.release()
+        break
+        
--- projection.py
+++ projection.py
@@ -1,28 +1,54 @@
 import cv2
 import numpy as np
 
-def projectImage(img1, img2, homography, thresh = 10, maxval = 255):
-
-    w2, h2 = img1.shape
+def projectImage(img1, img2, homography, thresh, maxval, transform, transformed):
+    
+    w2, h2, d2 = img2.shape
+    
+    if transformed:
+        img2 = cv2.warpPerspective(img2,transform,(h2,w2))
+    
+    w2, h2, d2 = img1.shape
+    #w2, h2, d2 = img1.shape
     img2_warped = cv2.warpPerspective(img2, homography, dsize=(h2, w2))
     #cv2.imshow('before',img2)
     #cv2.imshow('after',img2_warped)
     #cv2.waitKey(0)
-    ret, mask = cv2.threshold(img2_warped, thresh, maxval, cv2.THRESH_BINARY)
+    
+    img2_warped_gray = cv2.cvtColor(img2_warped, cv2.COLOR_BGR2GRAY)
+    ret, mask = cv2.threshold(img2_warped_gray, thresh, maxval, cv2.THRESH_BINARY)
+  
+    kernel = np.ones((20,20), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=3)
+    
+#    print(type(mask))
+#    print(img1.shape)
+#    print(img2_warped.shape)
+#    print(mask.shape)
+    
     inv = cv2.bitwise_not(mask)
-    img1_bg = cv2.bitwise_and(img1, inv)
-    img2_fg = cv2.bitwise_and(img2_warped, mask)
-
-    return cv2.add(img1_bg, img2_fg)
+    
+    img1_bg = cv2.bitwise_and(img1,img1,mask = inv)
+    img2_fg = cv2.bitwise_and(img2_warped,img2_warped, mask = mask)
+#    temp = cv2.add(mask,inv)
+#    cv2.imshow('masks',temp)
+#    cv2.imshow('mask',mask)
+#    cv2.imshow('inv',inv)
+#    cv2.imshow('warped',img2_warped)
+#    cv2.imshow('warped gray',img2_warped_gray)
+#    cv2.imshow('bg', img1_bg)
+#    cv2.imshow('fg',img2_fg)
+#    cv2.waitKey(0)
+    result = cv2.add(img1_bg, img2_fg)
+    
+    
+    return result
 
 
 # Find good matches between images, and return them
-def findGoodMatches(des1, des2):
+def findGoodMatches(des1, des2, matchDetector):
     # FLANN: Fast Library for Approximate Nearest Neighbour
-    FLANN_INDEX_KDTREE = 0
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
-    matchDetector = cv2.FlannBasedMatcher(index_params, search_params)
+    
 
     matches = matchDetector.knnMatch(des1, des2, k = 2)
     goodMatches = []
@@ -43,3 +69,35 @@ def findHomography(kp1, kp2, goodMatches):
     homography, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
     
     return homography
+
+def rotateMatrix(theta,image):
+
+    
+    angle = (-theta)*np.pi/180
+    w,h, d = image.shape
+    
+    A1 = np.array([[1,0,-w/2], \
+                   [0,1,-h], \
+                   [0,0,0], \
+                   [0,0,1.0]])
+    
+    rmatrix = np.array([[1,0,0,0], \
+                   [0,np.cos(angle),-np.sin(angle),0], \
+                   [0,np.sin(angle),np.cos(angle),0], \
+                   [0,0,0,1.0]])
+    
+    T = np.array([[1,0,0,0],\
+                  [0,1,0,0],\
+                  [0,0,1,300],\
+                  [0,0,0,1]])
+    
+    A2 = np.array([[200,0,w/2,0], \
+                   [0,200,h/2,0], \
+                   [0,0,1.0,0]]) \
+                   
+                   
+    transform = A2 @ ( T@ (rmatrix@ A1))
+    
+    img2 = cv2.warpPerspective(image, transform,(h,int(w/2)), cv2.INTER_LANCZOS4)
+    
+    return transform, img2
